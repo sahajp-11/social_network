0321a66695ddbfccf26ccb245b6c578c5dc044e7
Sahaj Patel <sahajp03@gmail.com>
2024-03-12 09:50:00 -0400

    added the social network python file

---
+++ social_network.py
@@ -0,0 +1,17 @@
+"""An application for tracking social network"""
+
+
+class Person:
+    """A class for tracking people and their connection
+    Attributes:
+    
+        name(str): name of the person
+        connections (set of connections): a set of people that a person knows
+        
+    
+    """
+    
+    def __init__(self, name):
+        
+        self.name = name
+        self.connections = set()
